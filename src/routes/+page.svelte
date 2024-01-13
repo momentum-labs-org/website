90eb017ead55194a3f38c45dd4cdc946bd921d35
Stefan Buhrmester <buhrmi@gmail.com>
2024-01-13 19:38:32 +0100

    netlify

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<script>
+  import '@unocss/reset/tailwind.css'
+  import 'virtual:uno.css'
+</script>
+
+<div class="container mx-auto">
+<h1>Welcome to Momentum Labs</h1>
+
+<p>
+  We create <strong>Momentum</strong>, the definite blockchain platform for decentralized apps and games.
+</p>
+<p>
+  Stay tuned for more information.
+</p>
+</div>
+
+<style>
+  h1 {
+    font-size: 3rem;
+    font-weight: 700;
+    margin-bottom: 1rem;
+  }
+  h2 {
+    font-size: 2rem;
+    font-weight: 500;
+    margin-bottom: 1rem;
+  }
+  p {
+    font-size: 1.5rem;
+    font-weight: 400;
+    margin-bottom: 1rem;
+  }
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+
+</style>
